ebc606ee9b818b571c0555c879a3e6621395b360
Nishu <nimish.prabhakar@eagleview.com>
2021-03-31 17:17:59 +0530

    Added new components to expense page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Container,
+  Header,
+  Statistic,
+  Segment,
+  Grid,
+  Icon,
+  Form,
+  Button,
+} from 'semantic-ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header as="h1">Budget</Header>
+      <Statistic size="small">
+        <Statistic.Label>Your Balance</Statistic.Label>
+        <Statistic.Value>2,204</Statistic.Value>
+      </Statistic>
+      <Segment textAlign="center">
+        <Grid columns={2} divided>
+          <Grid.Row>
+            <Grid.Column>
+              <Statistic size="tiny" color="green">
+                <Statistic.Label style={{ textAlign: 'left' }}>
+                  Income:
+                </Statistic.Label>
+                <Statistic.Value>1000</Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+            <Grid.Column>
+              <Statistic size="tiny" color="red">
+                <Statistic.Label style={{ textAlign: 'left' }}>
+                  Expenses:
+                </Statistic.Label>
+                <Statistic.Value>623.50</Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Header as="h3">History</Header>
+      <Segment color="red">
+        <Grid columns={3} textAlign="right" divided>
+          <Grid.Row>
+            <Grid.Column width={10} textAlign="left">
+              Chocolate
+            </Grid.Column>
+            <Grid.Column width={3} textAlign="right">
+              $10.0
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name="edit" />
+              <Icon name="trash" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Segment color="yellow">
+        <Grid columns={3} textAlign="right" divided>
+          <Grid.Row>
+            <Grid.Column width={10} textAlign="left">
+              Clothes
+            </Grid.Column>
+            <Grid.Column width={3} textAlign="right">
+              $100.0
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name="edit" />
+              <Icon name="trash" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Segment color="blue">
+        <Grid columns={3} textAlign="right" divided>
+          <Grid.Row>
+            <Grid.Column width={10} textAlign="left">
+              Pastry
+            </Grid.Column>
+            <Grid.Column width={3} textAlign="right">
+              $25.0
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name="edit" />
+              <Icon name="trash" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Header as="h3">Add New Transaction</Header>
+      <Form unstackable>
+        <Form.Group>
+          <Form.Input
+            icon="tags"
+            width={12}
+            placeholder="Add An Item"
+            label="Description"
+          ></Form.Input>
+          <Form.Input
+            width={4}
+            label="Value"
+            placeholder="Add the Value"
+            icon="dollar"
+            iconPosition="left"
+          />
+        </Form.Group>
+        <Button.Group style={{ marginTop: '20px' }}>
+          <Button>Cancel</Button>
+          <Button.Or></Button.Or>
+          <Button primary>Ok</Button>
+        </Button.Group>
+      </Form>
+    </Container>
   );
 }
 
